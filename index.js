1b266dd103a269e8c1ee1a3595fbeb3209414c17
Brianna-D <brianna.dever3@gmail.com>
2025-03-02 17:19:26 -0500

    We are finished

--- index.js
+++ index.js
@@ -5,9 +5,21 @@ function getRandomDog(){
     }).then((data) => {
         console.log(data.message);
         document.querySelector("#dog-image").src = data.message;
-    });
+    })
 }
 
 function getSpecificDog() {
-
+    let breed = document.querySelector("#breed").value;
+    fetch(`https://dog.ceo/api/breed/${breed}/images/random`).then((res) => {
+        if (!res.ok) {
+            // an error has happened, we should do something
+            alert("Woof! The dog breed you serched doesn't exist, try again")
+            return;
+        }
+        console.log(res);
+        return res.json();
+    }).then((data)=> {
+        console.log(data);
+        document.querySelector("#dog-image").src = data.message;
+})
 }
